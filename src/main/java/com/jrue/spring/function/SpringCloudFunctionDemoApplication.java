c966645ffc23a5f71c7eebded31fc0d11a6cb74f
Ruelos Joel <ruelosjoel@gmail.com>
2021-11-19 23:47:23 +0800

    Initial commit

---
+++ src/main/java/com/jrue/spring/function/SpringCloudFunctionDemoApplication.java
@@ -0,0 +1,23 @@
+package com.jrue.spring.function;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Function;
+
+@SpringBootConfiguration
+public class SpringCloudFunctionDemoApplication implements ApplicationContextInitializer<GenericApplicationContext> {
+
+    public Function<String, String> sayHello() {
+        return name -> String.format("Hello %s", name);
+    }
+
+    @Override
+    public void initialize(GenericApplicationContext applicationContext) {
+        applicationContext.registerBean("sayHello", FunctionRegistration.class,
+                () -> new FunctionRegistration<>(sayHello()).type(FunctionType.from(String.class).to(String.class)));
+    }
+}
---
+++ src/test/java/com/jrue/spring/function/SpringCloudFunctionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrue.spring.function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFunctionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

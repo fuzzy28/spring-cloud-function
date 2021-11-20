ede170086885fc9e9fa07e586251cdca4c1360ad
Ruelos Joel <ruelosjoel@gmail.com>
2021-11-20 16:17:55 +0800

    Back to functional style

--- src/main/java/org/jrue/poc/SpringCloudFunctionDemoApplication.java
+++ src/main/java/org/jrue/poc/SpringCloudFunctionDemoApplication.java
@@ -1,19 +1,28 @@
 package org.jrue.poc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.function.Function;
 
-@SpringBootApplication
-public class SpringCloudFunctionDemoApplication implements Function<String, String> {
+@SpringBootConfiguration
+public class SpringCloudFunctionDemoApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudFunctionDemoApplication.class, args);
     }
 
+    public Function<String, String> sayHello() {
+        return name -> String.format("Hello %s", name);
+    }
+
     @Override
-    public String apply(String input) {
-        return String.format("Hello %s", input);
+    public void initialize(GenericApplicationContext applicationContext) {
+        applicationContext.registerBean("sayHello", FunctionRegistration.class,
+                () -> new FunctionRegistration<>(sayHello()).type(FunctionType.from(String.class).to(String.class)));
     }
 }
